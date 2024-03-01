76cfeca6348f725e2b58ff56574ad035a3fb22f1
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-03-01 17:18:43 +0700

    Completed Kitten app to work with normal HTML

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -14,8 +14,9 @@ class KittensController < ApplicationController
   def create
     @kitten = Kitten.new(kitten_params)
     if @kitten.save
-      redirect_to kitten_path(@kitten)
+      redirect_to kitten_path(@kitten), notice: "New kitten has been created"
     else
+      flash.now[:alert] = "Invalid input"
       render :new, status: :unprocessable_entity
     end
   end
@@ -27,20 +28,21 @@ class KittensController < ApplicationController
   def update
     @kitten = Kitten.find(params[:id])
     if @kitten.update(kitten_params)
-      redirect_to kitten_path(@kitten)
+      redirect_to kitten_path(@kitten), notice: "Kitten has been updated"
     else
+      flash.now[:alert] = "Invalid input"
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     Kitten.find(params[:id]).destroy
-    redirect_to kittens_path
+    redirect_to kittens_path, notice: "Kitten has been deleted"
   end
 
   private
 
   def kitten_params
-    params.require(:kittens).permit(:name, :age, :cuteness, :softness)
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
